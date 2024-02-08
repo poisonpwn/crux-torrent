321e509a9654da518395614e6100dc944cd18659
Adithyadev R <45178461+poisonpwn@users.noreply.github.com>
2024-02-08 22:05:33 +0530

    feat(cli): add port argument

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,20 @@
-use anyhow;
 use clap::{self, Parser};
 
 mod torrent;
 use torrent::TorrentFilePath;
 
 #[derive(Parser, Debug)]
-#[command(author, about)]
+#[command(author, about, long_about = None)]
+/// a cli bittorrent (v1) client written in rust.
 struct Cli {
     #[arg(required = true)]
+    /// the source for the torrent information, i.e a torrent file.
+    /// torrent files must have the .torrent extention
     source: TorrentFilePath,
+
+    #[arg(short, long, default_value = "8860")]
+    /// the port on which to listen to incoming messages.
+    port: u16,
 }
 
 fn main() -> Result<(), anyhow::Error> {
