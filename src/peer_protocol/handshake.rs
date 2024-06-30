cdd37cb8dc3de92a38569a8d852c29136df97e4f
Adithyadev R <45178461+poisonpwn@users.noreply.github.com>
2024-06-30 15:44:43 +0530

    feat(handshake): add warning when protocol header doesn't match in peer handshake

--- src/peer_protocol/handshake.rs
+++ src/peer_protocol/handshake.rs
@@ -1,3 +1,4 @@
+use crate::prelude::*;
 use crate::torrent::{InfoHash, PeerId};
 
 #[derive(Debug, Clone, PartialEq)]
@@ -8,8 +9,8 @@ pub struct PeerHandshake {
     protocol_prefix_length: u8,
     protocol_prefix: [u8; 19],
     reserved_bytes: [u8; 8],
-    info_hash: InfoHash,
-    peer_id: PeerId,
+    pub info_hash: InfoHash,
+    pub peer_id: PeerId,
 }
 
 impl PeerHandshake {
@@ -26,7 +27,16 @@ impl PeerHandshake {
 
     // the unsafe is fine becuase the struct is just plain old data, any sequence of bits is valid.
     pub fn from_bytes(bytes: [u8; std::mem::size_of::<Self>()]) -> Self {
-        unsafe { std::mem::transmute::<[u8; std::mem::size_of::<Self>()], Self>(bytes) }
+        let handshake =
+            unsafe { std::mem::transmute::<[u8; std::mem::size_of::<Self>()], Self>(bytes) };
+        if handshake.protocol_prefix != Self::PROTOCOL_PREFIX {
+            warn!(
+                "unknown protocol prefix in handshake '{}'",
+                String::from_utf8_lossy(&handshake.protocol_prefix[..])
+            );
+        }
+
+        handshake
     }
     // the unsafe is fine becuase the struct is just plain old data, any sequence of bits is valid.
     pub fn into_bytes(self) -> [u8; std::mem::size_of::<Self>()] {
