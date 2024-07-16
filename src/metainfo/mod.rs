96882e6478e4932fba251e39e56fb6cfaf2855d5
Adithyadev R <45178461+poisonpwn@users.noreply.github.com>
2024-07-17 00:31:43 +0530

    refactor(metainfo/metainfo): move Metainfo struct into its own module metainfo

--- src/metainfo/mod.rs
+++ src/metainfo/mod.rs
@@ -1,42 +1,5 @@
 pub mod files;
+mod metainfo;
 pub mod url;
 
-use serde::Deserialize;
-use std::fs;
-use std::path::Path;
-use url::TrackerUrl;
-
-#[derive(Debug, Deserialize)]
-pub struct Metainfo {
-    pub announce: TrackerUrl,
-
-    #[serde(rename = "info")]
-    pub file_info: files::FileInfo,
-
-    #[serde(default)]
-    #[serde(rename = "announce-list")]
-    pub announce_list: Option<Vec<Vec<String>>>,
-
-    #[serde(default)]
-    #[serde(rename = "creation date")]
-    pub creation_date: Option<u64>, // seconds since unix epoch
-    //
-    #[serde(default)]
-    #[serde(rename = "created by")]
-    pub created_by: Option<String>,
-
-    #[serde(default)]
-    pub comment: Option<String>,
-
-    #[serde(default)]
-    pub encoding: Option<String>,
-}
-
-impl Metainfo {
-    pub fn from_bencode_file(file: impl AsRef<Path>) -> anyhow::Result<Self> {
-        let file_contents = fs::read(file)?;
-        let metainfo: Metainfo =
-            serde_bencode::from_bytes(&file_contents).map_err(anyhow::Error::msg)?;
-        Ok(metainfo)
-    }
-}
+pub use metainfo::Metainfo;
