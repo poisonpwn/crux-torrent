98723158f698bbcdc12c4bee5ea7d8b59f1fdeea
Adithyadev R <45178461+poisonpwn@users.noreply.github.com>
2024-07-16 23:59:11 +0530

    refactor(MetainfoFilePath): move MetainfoFilePath to cli.rs and make inner field private

--- src/cli.rs
+++ src/cli.rs
@@ -1,6 +1,47 @@
 use clap::{self, Parser};
 
-use crate::metainfo::MetainfoFilePath;
+use std::ffi::OsStr;
+use std::path::Path;
+use std::path::PathBuf;
+use std::str::FromStr;
+
+#[derive(Debug, Clone)]
+pub struct MetainfoFilePath(PathBuf);
+
+impl MetainfoFilePath {
+    pub fn new(path: impl Into<PathBuf>) -> Result<Self, anyhow::Error> {
+        let path: PathBuf = path.into();
+
+        if !path.is_file() {
+            anyhow::bail!("could not find file at {}", path.display());
+        }
+
+        let extension_is_torrent = path
+            .extension() // must have extension
+            .is_some_and(|s| s == OsStr::new("torrent"));
+
+        if !extension_is_torrent {
+            anyhow::bail!("torrent files must end have a .torrent extension");
+        }
+
+        Ok(MetainfoFilePath(path))
+    }
+}
+
+impl FromStr for MetainfoFilePath {
+    type Err = anyhow::Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let path = PathBuf::from(s);
+        Self::new(path)
+    }
+}
+
+impl AsRef<Path> for MetainfoFilePath {
+    fn as_ref(&self) -> &Path {
+        self.0.as_ref()
+    }
+}
 
 #[derive(Parser, Debug)]
 #[command(author, about, long_about = None)]
