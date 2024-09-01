2a54668be5c0f4b15604c4839e2512dfd79b63e3
Adithyadev R <45178461+poisonpwn@users.noreply.github.com>
2024-09-02 01:21:39 +0530

    index on test: 7df4cda test: add test for PeerId creation

--- src/peers/mod.rs
+++ src/peers/mod.rs
@@ -7,6 +7,12 @@ mod worker_fsm;
 
 pub use comms::*;
 
+use tokio::io::{AsyncRead, AsyncWrite};
+
+pub trait PeerStream: AsyncWrite + AsyncRead + Unpin {}
+impl<T: AsyncWrite + AsyncRead + Unpin> PeerStream for T {}
+
+pub type PeerAddr = std::net::SocketAddrV4;
 pub type PieceIndex = usize;
 pub type PieceLength = u32;
 type BlockLength = u32;
