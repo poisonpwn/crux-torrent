8a0fa591b4aad2cf3f34ef336413611801c15fb4
Adithyadev R <45178461+poisonpwn@users.noreply.github.com>
2025-07-26 02:37:54 +0530

    feat: split piece queue into a lockpool and a btree queue
    - to pick the next piece each peer iterates through the current btree
      queue and tries to acquire a lock from the lock pool for a piece
      index, if a lock is acquired it returns a piece handle containing a notify
      and a mpsc::Sender<PieceDone> to send the piece back to the piece
      picker which removes the piece from the queue and calls notify_one to
      instruct the worker to drop the piece handle containing the lock
      gaurd, the notify is there so that the gaurd is not reacquired between
      the piece being sent and it being removed from the queue.
    
    - alias Bitslice to the appropriate type in crate::torrent.

--- src/piece_picker/piece_picker_handle.rs
+++ src/piece_picker/piece_picker_handle.rs
@@ -1,14 +1,45 @@
-use super::{PieceDone, PieceInfo, PieceQueue};
-use crate::prelude::*;
+use lockable::LockPool;
+
+use super::{PieceDone, PieceGaurd, PieceInfo, PieceQueue};
+use crate::{metainfo::PieceHash, peers::PieceIndex, prelude::*, torrent::Bitslice};
 use std::{
-    sync::{mpsc, Arc, Mutex, MutexGuard, RwLock, TryLockError},
+    sync::{Arc, RwLock},
     time::Duration,
 };
+use tokio::sync::Notify;
 
-#[derive(Debug, Clone)]
+use tokio::sync::mpsc;
+
+#[derive(Clone)]
 pub struct PiecePickerHandle {
     piece_queue: Arc<RwLock<PieceQueue>>,
+    lock_pool: Arc<LockPool<PieceIndex>>,
     piece_tx: mpsc::Sender<PieceDone>,
+}
+
+pub struct PieceHandle<'a> {
+    pub piece_id: PieceIndex,
+    pub piece_length: u32,
+    pub piece_hash: PieceHash,
+    _gaurd: PieceGaurd<'a>,
+    piece_tx: mpsc::Sender<PieceDone>,
+}
+
+impl PieceHandle<'_> {
+    pub async fn submit(self, piece: Vec<u8>) -> anyhow::Result<()> {
+        let notify = Arc::new(Notify::new());
+        self.piece_tx
+            .send(PieceDone {
+                piece_id: self.piece_id,
+                piece,
+                notify: notify.clone(),
+            })
+            .await?;
+
+        notify.notified().await;
+        debug!("received drop notify: {}", self.piece_id);
+        Ok(())
+    }
 }
 
 impl PiecePickerHandle {
@@ -16,43 +47,60 @@ impl PiecePickerHandle {
 
     pub(super) fn new(
         piece_queue: Arc<RwLock<PieceQueue>>,
+        lock_pool: Arc<LockPool<PieceIndex>>,
         piece_tx: mpsc::Sender<PieceDone>,
     ) -> Self {
         Self {
             piece_queue,
             piece_tx,
+            lock_pool,
         }
     }
 
-    async fn next_piece(&mut self) -> anyhow::Result<MutexGuard<'a, PieceInfo>> {
+    #[instrument("next piece", level = "debug", skip_all)]
+    pub async fn next_piece(&self, bitfield: &Bitslice) -> anyhow::Result<PieceHandle<'_>> {
+        debug!("acquiring next piece to download from piece picker");
         loop {
-            let queue = self.piece_queue.read()?;
+            {
+                let queue = self.piece_queue.read().map_err(|_| {
+                    error!("error encountered while trying to acquire lock to read piece queue");
+                    anyhow::Error::msg(
+                        "error encountered while trying to acquire lock to read piece queue",
+                    )
+                })?;
 
-            for piece_info in queue.values() {
-                match piece_info.try_lock() {
-                    Ok(res) => return Ok(res),
-                    Err(TryLockError::Poisoned(e)) => return Ok(e.into_inner()),
-                    _ => continue,
+                if queue.is_empty() {
+                    debug!("piece picker queue is empty!");
+                } else {
+                    debug!(piece_picker_queue_size = queue.len());
+                }
+
+                for (piece_id, piece_info) in queue.iter() {
+                    if !bitfield.get(*piece_id).is_some_and(|bitref| *bitref) {
+                        continue;
+                    }
+
+                    if let Some(gaurd) = self.lock_pool.try_lock(*piece_id) {
+                        debug!("lock acquired for piece: {}", *piece_id);
+                        debug!(result = self.lock_pool.try_lock(*piece_id).is_none());
+                        let PieceInfo {
+                            piece_id,
+                            hash,
+                            length,
+                        } = *piece_info;
+                        return Ok(PieceHandle {
+                            piece_id,
+                            piece_hash: hash,
+                            piece_length: length,
+                            _gaurd: gaurd,
+                            piece_tx: self.piece_tx.clone(),
+                        });
+                    }
                 }
             }
 
+            debug!("no pieces free to be downloaded");
             tokio::time::sleep(Self::IDLE_CHECK_WAIT_DURATION).await;
         }
-    }
-
-    async fn send_piece(
-        &mut self,
-        piece_info: MutexGuard<'a, PieceInfo>,
-        piece: Vec<u8>,
-    ) -> anyhow::Result<()> {
-        debug!(
-            "sending done piece to piece_picker: {}",
-            piece_info.piece_id
-        );
-        self.piece_tx.send(PieceDone {
-            piece_id: piece_info.piece_id,
-            piece,
-        })?;
-        Ok(())
     }
 }
--- src/torrent/mod.rs
+++ src/torrent/mod.rs
@@ -2,6 +2,6 @@ mod bitfield;
 mod info_hash;
 mod peer_id;
 
-pub use bitfield::Bitfield;
+pub use bitfield::{Bitfield, Bitslice};
 pub use info_hash::InfoHash;
 pub use peer_id::PeerId;
