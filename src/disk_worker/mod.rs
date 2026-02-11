dd3aec7ddbc04327ca9b2152de0faa77d8419a4d
Adithyadev R <45178461+poisonpwn@users.noreply.github.com>
2026-02-11 18:13:04 +0530

    feat: rewrite (smol) disk_worker to just use memory mapped files instead of NOITrees and buffer flushes
    - note this was decided because of the rather flaky alternatives on
      windows for the pwritev syscall, i figured it would be better and
      perhaps more performant for the kernel to manage flushes at it's own
      discretion, and maybe also triggering flush periodically on a seperate
      thread.

--- src/disk_worker/mod.rs
+++ src/disk_worker/mod.rs
@@ -1,65 +1,100 @@
+use memmap2::MmapMut;
+use std::collections::BTreeMap;
 use std::path::PathBuf;
+use tokio_util::bytes::Bytes;
 
-use crate::torrent::{PieceIndex, PieceLength};
-mod noitree;
-
-struct FileSlice {
-    file_offset: u32,
-}
+use crate::torrent::{Piece, PieceLength, TorrentLength};
+use std::fs::OpenOptions;
 
 struct FileInfo {
     path: PathBuf,
     md5sum: Option<String>,
-}
-
-struct Piece {
-    index: PieceIndex,
-    data: Vec<u8>,
+    store: MmapMut,
 }
 
 struct DiskWorker {
+    files: BTreeMap<usize, FileInfo>, // file_starts -> FileInfo mapping, sorted ascending by file starts
     piece_length: PieceLength,
-    torrent_length: usize,
+    torrent_length: TorrentLength,
 }
 
 impl DiskWorker {
-    // fn new(
-    //     fileinfos: &[crate::metainfo::FileInfo],
-    //     piece_length: PieceLength,
-    // ) -> anyhow::Result<(DiskWorker, mpsc::Sender<Piece>)> {
-    //     let files = BTreeMap::new();
-    //     let start = 0;
-    //     for file in fileinfos {
-    //         let len = file.length;
-    //         files[(start, start + len)] = FileInfo {
-    //             path: PathBuf::from(file.path),
-    //             md5sum: file.md5sum,
-    //         };
-    //         start += len;
-    //     }
-    //     let torrent_length = start;
-    //     let (piece_tx, piece_rx) = mpsc::channel();
-    //
-    //     let worker = DiskWorker {
-    //         files,
-    //         piece_rx,
-    //         piece_length,
-    //         torrent_length,
-    //     };
-    //
-    //     (worker, tx)
-    // }
+    fn new(
+        fileinfos: &[crate::metainfo::FileInfo],
+        piece_length: PieceLength,
+    ) -> anyhow::Result<Self> {
+        let mut file_starts = BTreeMap::new();
+        let mut start = 0;
+        for file in fileinfos {
+            let len = file.length;
 
-    // async fn run(&mut self) {
-    //     tokio::select! {
-    //         Some(piece) = self.piece_rx.recv() => {
-    //             self.handle_piece(piece);
-    //         }
-    //     }
-    // }
+            // FIXME: this concat is probably a bug?, check if it is actually valid.
+            let path = PathBuf::from(file.path[..].concat());
+            let store = unsafe {
+                // SAFETY: todo!() guarentee that this file is only modified by this program/thread
+                // ONLY, otherwise it will be unsafe and (might) crash
+                MmapMut::map_mut(
+                    &OpenOptions::new()
+                        .read(true)
+                        .write(true)
+                        .create(true)
+                        .truncate(false)
+                        .open(&path)?,
+                )?
+            };
 
-    // fn handle_piece(&mut self, piece: Piece) {
-    //     let begin = self.piece_length * piece.index;
-    //     let end = begin + piece.data.len();
-    // }
+            file_starts.insert(
+                start,
+                FileInfo {
+                    md5sum: file.md5sum.clone(),
+                    store,
+                    path,
+                },
+            );
+            start += len;
+        }
+
+        let torrent_length = start;
+
+        let worker = DiskWorker {
+            files: file_starts,
+            piece_length,
+            torrent_length,
+        };
+
+        Ok(worker)
+    }
+
+    fn write_piece(&mut self, piece: Piece) -> anyhow::Result<()> {
+        let piece_start = piece.piece_id * self.piece_length as usize;
+        let piece_end = piece.piece_id + piece.data.len();
+
+        let mut piece = Bytes::from(piece.data); // this makes it zero copy and mostly zero alloc.
+        let first = *self
+            .files
+            .range(..=piece_start)
+            .next_back()
+            .ok_or_else(|| anyhow::anyhow!("piece found to fall outside torrent range"))?
+            .0;
+
+        let mut iter = self.files.range_mut(&first..&piece_end).peekable();
+        let mut cut_total = 0;
+
+        // this has to be a while let Some loop because of the mutable borrow of the iterator.
+        while let Some((start, file_info)) = iter.next() {
+            let chunk_start = std::cmp::min(*start, piece_start);
+            let chunk_end = if let Some((end, _)) = iter.peek() {
+                **end
+            } else {
+                piece_end
+            };
+            let chunk = piece.split_to(chunk_end - chunk_start);
+            cut_total += chunk_end - chunk_start;
+
+            file_info.store[(chunk_start - start)..(chunk_start - start)].copy_from_slice(&chunk);
+        }
+
+        debug_assert!(cut_total == (piece_start - piece_end));
+        Ok(())
+    }
 }
